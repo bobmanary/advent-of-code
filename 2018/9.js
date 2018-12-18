83b0dcb7cfdb7836a6dfcd17c852be294c7221ce
Bob Manary <rmanary@gmail.com>
2018-12-17 22:54:22 -0500

    Day 9 part 2 (with fast linked list)

--- 2018/9.js
+++ 2018/9.js
@@ -5,6 +5,7 @@ const realData = parseLine(
   .trim()
 )
 const {FilledArray, strpad, print} = require('./lib.js')
+const process = require('process')
 
 const DRAW=false
 
@@ -36,25 +37,30 @@ const CCW = -1 // counter-clockwise
 const CW = 1
 
 function drawGameBoard(marbles, players) {
-  let heading = strpad(`[${strpad((players.current()+1) + ':', 4)} ${strpad(players.currentScore() + '', 6)}]`, 10)
+  let playerName = strpad((players.current()+1) + ':', 4)
+  let score = strpad(players.currentScore() + '', 6)
+  let heading = strpad(`[${playerName} ${score}]`, 10)
 
-  let text = marbles.map((m, i) => {
-    let pad = i === marbles.currentIndex() ? "()" : "  "
+  let gameBoard = marbles.map((m, i) => {
+    let pad = m === marbles.currentValue() ? "()" : "  "
     return strpad(`${pad[0]}${m}${pad[1]}`, 5)
   }).join(' ')
 
-  return `${heading} ${text}`
+  return `${heading} ${gameBoard}`
 }
+
+
 
 const currentMarbleIdx = Symbol()
 class Marbles extends Array {
   constructor() {
     super()
-    this[currentMarbleIdx] = -1
+    this[currentMarbleIdx] = 0
+    this.push(0)
   }
 
   placeMarble(marble) {
-    return this.insertRelative(marble)
+    this.insertRelative(marble)
   }
 
   removeMarble() {
@@ -103,6 +109,86 @@ class Marbles extends Array {
   currentIndex() {
     return this[currentMarbleIdx]
   }
+
+  currentValue() {
+    return this[this.currentIndex()]
+  }
+}
+
+
+class LinkedListMarbles {
+  constructor() {
+    let node = new Node(0)
+    node.ccw = node
+    node.cw = node
+    this.currentNode = node
+    this.length = 1
+    this.head = node
+  }
+
+  placeMarble(marble) {
+    let ccw = this.currentNode.cw
+    let cw = ccw.cw
+    let newNode = new Node(marble, ccw, cw)
+    ccw.cw = newNode
+    cw.ccw = newNode
+
+    this.length++
+    this.currentNode = newNode
+  }
+
+  removeMarble() {
+    let removedNode = this.currentNode.getCounterClockwise(7)
+    let ccw = removedNode.ccw
+    let cw = removedNode.cw
+    cw.ccw = cw
+    ccw.cw = cw
+    this.currentNode = cw
+    if (this.head === removedNode) this.head = cw
+    this.length--
+    return removedNode.value
+  }
+
+  currentValue() {
+    return this.currentNode.value
+  }
+
+  map(fn) {
+    let node = this.head
+    let i = 0
+    let mapped = []
+    do {
+      mapped.push(fn(node.value, i++))
+      node = node.cw
+    } while (node.cw != this.head)
+    return mapped
+  }
+}
+
+class Node {
+  constructor(value, ccw, cw) {
+    this.value = value
+    this.ccw = ccw
+    this.cw = cw
+  }
+
+  getClockwise(count = 1) {
+    let cw = this
+    while (count > 0) {
+      cw = cw.cw
+      --count
+    }
+    return cw
+  }
+
+  getCounterClockwise(count = 1) {
+    let ccw = this
+    while (count > 0) {
+      ccw = ccw.ccw
+      --count
+    }
+    return ccw
+  }
 }
 
 const playerIndex = Symbol()
@@ -131,16 +217,17 @@ class Players extends FilledArray {
   }
 }
 
-function part1({players: playerCount, lastMarble}) {
+function calculateFinalScore({players: playerCount, lastMarble}, MarblesImplementation = Marbles) {
   let moveLog = []
-  const marbles = new Marbles()
+  const marbles = new MarblesImplementation()
   const players = new Players(playerCount, 0)
-  marbles.placeMarble(0)
+
   const printStatus = (currentMarble) => {
-    process.stdout.clearLine()
-    process.stdout.cursorTo(0)
+    process.stdout.clearLine && process.stdout.clearLine()
+    process.stdout.cursorTo && process.stdout.cursorTo(0)
     print('  marble ' + currentMarble + '/' + lastMarble)
   }
+
   for(let currentMarble = 1; currentMarble <= lastMarble; ++currentMarble) {
     if (currentMarble % 23 === 0) {
       players.increase(currentMarble)
@@ -162,14 +249,20 @@ function part1({players: playerCount, lastMarble}) {
   return players.reduce((max, p) => p > max ? p : max, 0)
 }
 
-// console.log(testInputs[0])
-// assert.equal(part1(testInputs[0].input), testInputs[0].expectedScore)
 testInputs.forEach(({input, expectedScore}) => {
-  console.log(`testing part1(${JSON.stringify(input)}) == ${expectedScore}...`)
-  assert.equal(part1(input), expectedScore)
+  // console.log(`testing calculateFinalScore(${JSON.stringify(input)}, Marbles) == ${expectedScore}...`)
+  // assert.equal(calculateFinalScore(input), expectedScore)
+  // print(" ok.\n")
+  console.log(`testing part1(${JSON.stringify(input)}, LinkedListMarbles) == ${expectedScore}...`)
+  assert.equal(calculateFinalScore(input, LinkedListMarbles), expectedScore)
   print(" ok.\n")
 })
 
 console.log("calculating part 1...")
-const part1Result = part1(realData)
+const part1Result = calculateFinalScore(realData, LinkedListMarbles)
 print(` result: ${part1Result}\n`)
+assert.equal(424112, part1Result)
+
+console.log("calculating part 2...")
+const part2Result = calculateFinalScore({...realData, lastMarble: realData.lastMarble*100}, LinkedListMarbles)
+print(` result: ${part2Result}\n`)
