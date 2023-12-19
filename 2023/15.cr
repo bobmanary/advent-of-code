30f3be5ed52549ba4c5f414c78c33609195541e3
Bob Manary <rmanary@gmail.com>
2023-12-18 22:20:01 -0500

    Solve 2023 day 15 in Crystal

---
+++ 2023/15.cr
@@ -0,0 +1,77 @@
+["inputs/15_example.txt", "inputs/15.txt"].each do |filename| #
+  input = load(filename)
+
+  puts "#{filename} part 1: #{part1(input)}"
+
+  puts "#{filename} part 2: #{part2(input)}"
+end
+
+def load(filename)
+  File.read(filename).chomp.chars
+end
+
+def part1(input)
+  hash(input)
+end
+
+def hash(chars)
+  sum = 0
+  current = 0
+  chars.each do |char|
+    if char == ','
+      sum += current
+      current = 0
+      next
+    end
+
+    current += char.ord
+    current *= 17
+    current %= 256
+  end
+  sum + current
+end
+
+def part2(input)
+  boxes = Array(Array({String, UInt8})).new(256) { [] of {String, UInt8}}
+  label = [] of Char
+  current = 0
+
+  input.each_with_index do |char, i|
+    next if char.ascii_number? || char == ','
+    if char == '-'
+
+      box = boxes[current]
+      label_str = label.join
+      idx = box.index {|lens| lens[0] == label_str}
+      if !idx.nil?
+        box.delete_at(idx)
+      end
+      current = 0
+      label.clear
+    elsif char == '='
+      box = boxes[current]
+      lens_number = input[i+1].to_u8
+      label_str = label.join
+      idx = box.index {|lens| lens[0] == label_str}
+      if idx.nil?
+        box << {label_str, lens_number}
+      else
+        box[idx] = {label_str, lens_number}
+      end
+      current = 0
+      label.clear
+    else
+      current += char.ord
+      current *= 17
+      current %= 256
+      label << char
+    end
+  end
+
+  boxes.each_with_index.reduce(0) do |acc, (box, box_number)|
+    next acc if box.empty?
+    acc + box.each_with_index.reduce(0) do |acc2, ((label, focal_length), slot_number)|
+      acc2 + ((1 + box_number) * (1 + slot_number) * focal_length)
+    end
+  end
+end
